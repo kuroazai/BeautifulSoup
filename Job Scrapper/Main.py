92d6bf7144d679181e752c6c9304d4c0f7acd32f
kuroazai <pyromwansa@gmail.com>
2020-01-10 11:52:46 +0000

    Update Main.py
    Supports n number of keywords that user can input, recommended to create an encrypted Json file for ease of access.

--- Job Scrapper/Main.py
+++ Job Scrapper/Main.py
@@ -20,25 +20,79 @@ http = urllib3.PoolManager()
 
 
    
+def usr_inputs():
+    switch = True
+    keywords = []
+    print('''
+          Help : 
+          del - deletes last entry,
+          clr - clears the input list, 
+          shw - shows current entries, 
+          done - run with given parameters,
+          ''')
+    print("Please enter a Keyword / Location to append to search Critia \n \n")  
+
+    while switch == True:      
+        print("\n#",len(keywords) + 0, " :Waiting for input")
+        x = input()       
+        #Check contents 
+        if x == "del":
+            #remove last entry
+            keywords.pop()
+        if x == "clr":
+            #Empty the array
+            keywords = []
+        if x == "shw":
+            #Show all current entries
+            for y in keywords:
+                print(y)
+        if x == "done":
+            #break from loop
+            print("\n Completed\n\n")
+            break
+        elif x != "del" and x != "cls":
+            #append new input
+            keywords.append(x)
+                      
+    return keywords
+        
+    
 def Angel_Login():
+    
+    def json_login():
+        with open('data.json') as json_file:
+            data = json.load(json_file)
+        driver = browser
+        driver.find_element_by_id("user_email").send_keys(data['user[email]'])
+        driver.find_element_by_id ("user_password").send_keys(data['user[password]'])
+        driver.find_element_by_xpath('//input[@type="submit"]').click()
+        return driver
+        
+    def input_login():
+        data = []
+        print("Enter Email")
+        data.append(input())
+        print("Enter Password")
+        data.append(input()) 
+        driver = browser
+        driver.find_element_by_id("user_email").send_keys(data[0])
+        driver.find_element_by_id ("user_password").send_keys(data[1])
+        driver.find_element_by_xpath('//input[@type="submit"]').click()
+        return driver
+    
     url = "https://angel.co/login" 
     #Selenium FF browser options for headless 
     options = Options()
     #Run Driver in headerless mode
-    options.headless = True
+    options.headless = False
     #Set the driver with its options 
     browser = webdriver.Firefox(options=options)
     #Get the results from our headless browser for our objects page
     browser.get(url)
+         
+    #Choose login funciton, using Json file to avoid repetitveness of logins
+    driver = json_login()
     
-    #Open Json with login data
-    with open('data.json') as json_file:
-        data = json.load(json_file)
-        
-    driver = browser
-    driver.find_element_by_id("user_email").send_keys(data['user[email]'])
-    driver.find_element_by_id ("user_password").send_keys(data['user[password]'])
-    driver.find_element_by_xpath('//input[@type="submit"]').click()
     time.sleep(3)
     return driver
 
@@ -56,10 +110,12 @@ def Angel_JobScrap(driver,*args):
     driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]').click()
     searchbox = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/input')
     for arg in args:
-        searchbox.send_keys(arg)
-        searchbox.send_keys(Keys.RETURN)
+        for x in arg:
+            searchbox.send_keys(x)
+            searchbox.send_keys(Keys.RETURN)
     
     driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]').click()
+    time.sleep(2)
     #Scroll to the bottom of the page
     Scroll_Down(driver)
 
@@ -73,8 +129,8 @@ def Scroll_Down(driver):
 
     # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
-    
-    while True:
+    i = 0
+    while True and i < 10:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     
@@ -86,6 +142,7 @@ def Scroll_Down(driver):
         if new_height == last_height:
             break
         last_height = new_height
+        i += 1
         
 def Companies(soup):    
     jobs = soup.findAll("a", {"class": "startup-link"})
@@ -102,12 +159,14 @@ def kill_gecko():
     
 
 def main():
-    #Kill Previous Instances due to headless mode
+    #Get Inputs store them as keywords
+    kw = usr_inputs()
+    #Kill Previous Instances due to headless mode(Kills firefox instances)
     kill_gecko()
     #Login into angel list
     driver = Angel_Login()
     #Search for desired skills/locations
-    soup = Angel_JobScrap(driver,"Python","Oxford")
+    soup = Angel_JobScrap(driver,kw)
     #Process with soup
     Companies(soup)
 
